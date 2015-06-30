4afc8d4973adea31854d9a63d9231b9e3634ab44
nickqiao <393533945@qq.com>
2015-07-01 01:38:34 +0800

    DealHeaderView
    检查问题

---
+++ DZDP/DZDP/Classes/Deal/View/DPDealHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  DPDealHeaderView.h
+//  DZDP
+//
+//  Created by nickchen on 15/7/1.
+//  Copyright (c) 2015年 nickchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPDealHeaderView : UIView
+
+@end
---
+++ DZDP/DZDP/Classes/Deal/View/DPDealHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  DPDealHeaderView.m
+//  DZDP
+//
+//  Created by nickchen on 15/7/1.
+//  Copyright (c) 2015年 nickchen. All rights reserved.
+//
+
+#import "DPDealHeaderView.h"
+
+@implementation DPDealHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
